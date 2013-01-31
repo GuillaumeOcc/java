03d1ff3b568828afda2efbb712a5805ec1484789
Guillaume <guillaume@siu-soon.com>
2013-01-31 09:47:48 +0100

    2e message

---
+++ sdz/src/com/sdz/project1/sdz1.java
@@ -0,0 +1,27 @@
+package com.sdz.project1;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Guillaume
+ * Date: 30/01/13
+ * Time: 00:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class sdz1 {
+    public static void main(String[] args){
+
+        char tableauCaractere[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+        for (int i=0; i <tableauCaractere.length; i++){
+
+            System.out.println(tableauCaractere[i]);
+        }
+
+
+
+
+
+
+    }
+}
